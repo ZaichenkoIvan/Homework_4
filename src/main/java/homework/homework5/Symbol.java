71a4dedda20b85ab47a75bda822ac64e99cb641a
Ivan Zaichenko <i.v.zaichenko70@gmail.com>
2019-09-23 15:12:25 +0300

    Add Text task

---
+++ src/main/java/homework/homework5/Symbol.java
@@ -0,0 +1,14 @@
+package homework.homework5;
+
+public class Symbol {
+
+    char ch;
+
+    public Symbol(char ch) {
+        this.ch = ch;
+    }
+
+    public char get() {
+        return ch;
+    }
+}
